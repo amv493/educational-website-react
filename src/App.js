e339c8bab8fb56729772a1ccf22d47fd1ca8cc55
Anthony McKinney <amv493@gmail.com>
2024-03-03 16:05:16 -0500

    Added Courses Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Header from './components/common/heading/Header'
 import Home from './components/home/Home' 
 import About from "./components/about/About"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import CourseHome from "./components/allcourses/CourseHome"
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={ Home } />
           <Route path='/about' exact component={ About } />
+          <Route path='/courses' exact component={ CourseHome } />
         </Switch>
       </Router>
     </>
