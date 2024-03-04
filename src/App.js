893eaf7aafd12f2d5c1564aee5d609968de02501
Anthony McKinney <amv493@gmail.com>
2024-03-04 18:40:04 -0500

    Added teams section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/common/heading/Header'
 import Home from './components/home/Home' 
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' exact component={ Home } />
           <Route path='/about' exact component={ About } />
           <Route path='/courses' exact component={ CourseHome } />
+          <Route path='/team' exact component={ Team } />
         </Switch>
       </Router>
     </>
